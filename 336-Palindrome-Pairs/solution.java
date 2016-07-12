f9fced3479dc2583824df91973a39f50367c4ce8
Firkraag <cntqrxj@gmail.com>
2016-07-12 17:31:34 +0800

    [336. Palindrome Pairs][Time Limit Exceeded]committed by LeetCode Extension

--- 336-Palindrome-Pairs/solution.java
+++ 336-Palindrome-Pairs/solution.java
@@ -1,47 +1,187 @@
+class TrieNode {
+    private TrieNode[] child;
+    private int freq;
+    private int index;
+    private HashSet<Integer> set;
+    // Initialize your data structure here.
+    public TrieNode() {
+        child = new TrieNode[26];
+        freq = 0;
+        index = -1;
+        set = new HashSet<Integer>();
+    }
+
+    public TrieNode getIthChild(int i) {
+        return child[i];
+    }
+    public void setIthChild(int i)
+    {
+        child[i] = new TrieNode();
+    }
+    public int getFreq()
+    {
+        return freq;
+    }
+    public void increaseFreq(int delta)
+    {
+        freq += delta;
+    }
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public HashSet<Integer> getSet() {
+        return set;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public TrieNode getRoot() {
+        return root;
+    }
+    // Inserts a word into the trie.
+    public void insert(StringBuffer word, int idx) {
+        TrieNode node = root;
+        
+        for (int i = 0; i < word.length(); i++)
+        {
+            int index = word.charAt(i) - 'a';
+            node.getSet().add(new Integer(idx));
+            if (node.getIthChild(index) == null)
+            {
+                node.setIthChild(index);
+                node = node.getIthChild(index);
+            }
+            else
+                node = node.getIthChild(index);
+        }
+        node.increaseFreq(1);
+        node.setIndex(idx);
+    }
+
+}
+
 public class Solution {
     public List<List<Integer>> palindromePairs(String[] words) {
+        Trie trie = new Trie();
         int n = words.length;
-        boolean[][] v = new boolean[n][n];
-        HashMap<String, Integer> h = new HashMap();
-        List<List<Integer>> ans = new ArrayList();
-        for (int i = 0; i < words.length; h.put(words[i], i), v[i][i] = true, i++);
+        ArrayList<List<Integer>> pairs = new ArrayList<List<Integer>>();
+        ArrayList<ArrayList<Integer>> RLs = new ArrayList<ArrayList<Integer>>();
         
-        for (int i = 0; i < n; i++) {
-            List<String> palinMatch = findPalinMatch(words[i]);
-            for (String cand : palinMatch)
-                if (h.containsKey(cand) && !v[h.get(cand)][i]) {
-                    ans.add(Arrays.asList(h.get(cand), i));
-                    v[h.get(cand)][i] = true;
-                }
-                    
-            palinMatch = findPalinMatch(new StringBuilder(words[i]).reverse().toString());
-            for (String cand : palinMatch) {
-                String candR = new StringBuilder(cand).reverse().toString();
-                if (h.containsKey(candR) && !v[i][h.get(candR)]) {
-                    ans.add(Arrays.asList(i, h.get(candR)));
-                    v[i][h.get(candR)] = true;
-                }
-            }
+        for (int i = 0; i < n; i++)
+        {
+            trie.insert((new StringBuffer(words[i])).reverse(), i);
+            RLs.add(manacher(words[i]));
         }
-        
-        return ans;
+        for (int i = 0; i < n; i++)
+            findPair(words, trie, pairs, i, RLs);
+        return pairs;
     }
     
-    private List<String> findPalinMatch(String s) {
-        String y = s + "#" + new StringBuilder(s).reverse().toString();
-        int len = y.length();
-        int[] p = new int[len]; // find KMP table
-        p[0] = 0;
-        for (int i = 1; i < len; i++) {
-            int j = p[i - 1];
-            while (j > 0 && y.charAt(i) != y.charAt(j)) j = p[j - 1];
-            p[i] = (y.charAt(i) != y.charAt(j)) ? 0 : j + 1;
-        }
+    private void findPair(String[] words, Trie trie, ArrayList<List<Integer>> pairs, int wordIndex, ArrayList<ArrayList<Integer>> RLs)
+    {
+        String word = words[wordIndex];
+        TrieNode node = trie.getRoot();
+        int n = word.length();
+        int i = 0;
         
-        List<String> res = new ArrayList();
-        for (int j = p[len - 1]; j > 0; j = p[j - 1])
-            res.add(y.substring(len - s.length(), len - j));
-        res.add(y.substring(len - s.length()));
-        return res;    
+        while (i < n)
+        {
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if (node.getFreq() > 0)
+            {
+                if (wordIndex != node.getIndex() && isPalindrome(word, i, n - 1, RLs.get(wordIndex)))
+                {
+                    List<Integer> pair = new ArrayList<Integer>();
+                    pair.add(new Integer(wordIndex));
+                    pair.add(new Integer(node.getIndex()));
+                    pairs.add(pair);
+                }
+            }
+            if (node.getIthChild(index) == null)
+                return;
+            else
+            {
+                node = node.getIthChild(index);
+                i++;
+            }
+        }
+        postProcessing(node, pairs, words, wordIndex, RLs);
+        
     }
+    
+    private void postProcessing(TrieNode node, ArrayList<List<Integer>> pairs, String[] words, int wordIndex, ArrayList<ArrayList<Integer>> RLs)
+    {
+        if (node.getFreq() > 0 && wordIndex != node.getIndex())
+        {
+            List<Integer> pair = new ArrayList<Integer>();
+            pair.add(new Integer(wordIndex));
+            pair.add(new Integer(node.getIndex()));
+            pairs.add(pair);
+        }
+        int n = words[wordIndex].length();
+        for (int index: node.getSet())
+        {
+            String word = words[index];
+            if (isPalindrome(word, 0, word.length() - n - 1, RLs.get(index)))
+            {
+                List<Integer> pair = new ArrayList<Integer>();
+                pair.add(new Integer(wordIndex));
+                pair.add(new Integer(index));
+                pairs.add(pair);
+            }
+        }
+    }
+    
+    private ArrayList<Integer> manacher(String s)
+    {
+        StringBuilder str = new StringBuilder();
+        str.append('#');
+        for (int i = 0; i < s.length(); i++)
+        {
+            str.append(s.charAt(i));
+            str.append('#');
+        }
+        int MaxRight = 0;
+        int pos = 0;
+        int MaxLen = 0;
+        int[] rl = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (i < MaxRight)
+                rl[i] = Math.min(rl[2 * pos - i], MaxRight - i);
+            else
+                rl[i] = 1;
+            while (i -  rl[i] >= 0 && i + rl[i] < str.length() && str.charAt(i - rl[i]) == str.charAt(i + rl[i]))
+                rl[i] += 1;
+            if (rl[i] + i - 1 > MaxRight)
+            {
+                MaxRight = rl[i] + i - 1;
+                pos = i;
+            }
+            MaxLen = Math.max(MaxLen, rl[i]);
+        }
+        ArrayList<Integer> RL = new ArrayList<Integer>();
+        for (int i: rl)
+        {
+            RL.add(new Integer(i));
+        }
+        return RL;
+    }
+    
+    private boolean isPalindrome(String s, int start, int end, ArrayList<Integer> RL) {
+        int pos = start + end + 1;
+        return start >= (pos - RL.get(pos).intValue() + 1) / 2 && (pos + RL.get(pos).intValue() - 1) / 2 >= end;
+    }
+    
 }
