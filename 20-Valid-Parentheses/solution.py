04683afd94ed7c1a0b1ed8efcedd4bf6f052b06a
Firkraag <cntqrxj@gmail.com>
2016-07-14 15:41:49 +0800

    Update solution.py

--- 20-Valid-Parentheses/solution.py
+++ 20-Valid-Parentheses/solution.py
@@ -1,5 +1,25 @@
-public class Solution {
-    public boolean isValid(String s) {
-        
-    }
-}
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n = len(s)
+        stack = []
+        d = {'[': ']', '{':'}', '(':')'}
+        se = set('])}')
+        for i in range(n - 1, -1, -1):
+            if s[i] in se:
+                stack.append(s[i])
+            else:
+                if len(stack) == 0:
+                    return False
+                else:
+                    c = stack.pop()
+                    if c != d[s[i]]:
+                        return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+       
