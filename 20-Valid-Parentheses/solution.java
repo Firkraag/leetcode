139bd253b57b3b3844ac5ae93ad2b030b4349e1b
Firkraag <cntqrxj@gmail.com>
2016-07-14 15:42:52 +0800

    Update solution.java

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,5 +1,33 @@
 public class Solution {
     public boolean isValid(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<Character>();
+        HashMap<Character, Character> d = new HashMap<Character, Character>();
+        HashSet<Character> se = new HashSet<Character>();
+        d.put('[', ']');
+        d.put('{', '}');
+        d.put('(', ')');
+        se.add(']');
+        se.add(')');
+        se.add('}');
         
+        for (int i = n - 1; i >= 0; i--) {
+            if (d.containsKey(s.charAt(i))) {
+                if (stack.size() == 0)
+                    return false;
+                else {
+                    char c = stack.pop();
+                    if (c != d.get(s.charAt(i)))
+                        return false;
+                }
+            }
+            else {
+                stack.push(s.charAt(i));
+            }
+        }
+        if (stack.size() == 0)
+            return true;
+        else
+            return false;
     }
 }
