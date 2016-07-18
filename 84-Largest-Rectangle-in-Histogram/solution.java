a617c6a537524e66bbbbdf54e675ffa7258a7561
Firkraag <cntqrxj@gmail.com>
2016-07-18 22:57:04 +0800

    Update solution.java

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -1,16 +1,46 @@
 public class Solution {
     public int largestRectangleArea(int[] heights) {
-        int n = heights.length;
-        int maximum = 0;
-        for (int i = 0; i < n; i++) {
-            int borderHeight = Integer.MAX_VALUE;
-            int borderMax = Integer.MIN_VALUE;
-            for (int j = i; j >= 0; j--) {
-                borderHeight = Math.min(borderHeight, heights[j]);
-                borderMax = Math.max(borderHeight * (i - j + 1), borderMax);
+        return largestRectangleAreaAux(heights, 0, heights.length - 1);
+    }
+    private int largestRectangleAreaAux(int[] heights, int p, int r) {
+        if (p > r)
+            return 0;
+        else if (p == r)
+            return heights[p];
+        else {
+            int mid = (p + r) / 2;
+            int area1 = largestRectangleAreaAux(heights, p, mid);
+            int area2 = largestRectangleAreaAux(heights, mid + 1, r);
+            int minHeight = Math.min(heights[mid], heights[mid + 1]);
+            int area = 2 * minHeight;
+            int i = mid - 1;
+            int j = mid + 2;
+            while (i >= p && j <= r) {
+                if (heights[i] >= heights[j]) {
+                    minHeight = Math.min(heights[i], minHeight);
+                    area = Math.max(area, minHeight * (j - i));
+                    i--;
+                }
+                else {
+                    minHeight = Math.min(heights[j], minHeight);
+                    area = Math.max(area, minHeight * (j - i));
+                    j++;
+                }
             }
-            maximum = Math.max(borderMax, maximum);
+            if (i >= p) 
+                while (i >= p) {
+                    minHeight = Math.min(heights[i], minHeight);
+                    area = Math.max(area, minHeight * (r - i + 1));
+                    i--;
+                }
+            else {
+                while (j <= r) {
+                    minHeight = Math.min(heights[j], minHeight);
+                    area = Math.max(area, minHeight * (j - p + 1));
+                    j++;
+                }
+            }
+            return Math.max(area1, Math.max(area2, area));
         }
-        return maximum;
     }
 }
