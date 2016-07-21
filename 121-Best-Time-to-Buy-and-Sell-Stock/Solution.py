4c1c8ae062922658aa771d21fc65fd8316e5aaea
Firkraag <cntqrxj@gmail.com>
2016-07-21 15:58:36 +0800

    Create Solution.py

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/Solution.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        n = len(prices)
+        if n == 0:
+            return 0
+        one = [0] * n
+        minimum = prices[0]
+        for i in range(1, n):
+            if minimum < prices[i]:
+                one[i] = max(one[i - 1], prices[i] - minimum)
+            else:
+                minimum = prices[i]
+                one[i] = one[i - 1]
+            #print minimum, one
+        return one[-1]
