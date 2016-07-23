05d865c6572e0fc7df803246b5d564f966e96ae2
Firkraag <cntqrxj@gmail.com>
2016-07-23 16:34:08 +0800

    Update solution.java

--- 85-Maximal-Rectangle/solution.java
+++ 85-Maximal-Rectangle/solution.java
@@ -1,7 +1,36 @@
-class Solution(object):
-    def maximalRectangle(self, matrix):
-        """
-        :type matrix: List[List[str]]
-        :rtype: int
-        """
-        
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int m = matrix.length;
+        if (m == 0)
+            return 0;
+        int n = matrix[0].length;
+        if (n == 0)
+            return 0;
+        int maxArea = 0;
+        boolean[][] bool = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                bool[i][j] = true;
+            }
+        }
+        for (int l = 1; l <= m; l++) {
+            for (int i = 0; i < m - l + 1; i++) {
+                int j = i + l - 1;
+                int maxLength = 0;
+                int length = 0;
+                for (int k = 0; k < n; k++) {
+                    bool[i][k] = bool[i][k] && (matrix[j][k] == '1');
+                    if (bool[i][k])
+                        length++;
+                    else {
+                        maxLength = Math.max(maxLength, length);
+                        length = 0;
+                    }
+                }
+                maxLength = Math.max(maxLength, length);
+                maxArea = Math.max(maxArea, maxLength * l);
+            }
+        }
+        return maxArea;
+    }
+}
