5c9ab4cc52f21e001b2abcbba3f4af023ad9bb8e
Firkraag <cntqrxj@gmail.com>
2016-08-16 14:15:38 +0800

    Update Solution.java

--- 30-Substring-with-Concatenation-of-All-Words/Solution.java
+++ 30-Substring-with-Concatenation-of-All-Words/Solution.java
@@ -1,9 +1,10 @@
 public class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        int n = s.length();
-        if (words.length == 0)
+        if (s.length() == 0 || words.length == 0 || words[0].length() == 0)
             return new ArrayList<Integer>();
-        int length = words[0].length();
+        int n = s.length();
+        int wordLength = words[0].length();
+        int totalLength = words.length * words[0].length();
         List<Integer> result = new ArrayList<Integer>();
         Map<String, Integer> counter = new HashMap<String, Integer>();
         for (String word: words) {
@@ -14,27 +15,61 @@ public class Solution {
                 counter.put(word, 1);
             }
         }
-        System.out.println("length of s: " + n + " length of words: " + words.length + "length of word: " + length);
-        int c = 0;
-        for (int i = 0; i < n - length * words.length + 1; i++) {
+        //System.out.println("length of s: " + n + " length of words: " + words.length + "length of word: " + length);
+        for (int k = 0; k < wordLength; k++) {
+            if (k + totalLength > n) {
+                break;
+            }
             Map<String, Integer> number = new HashMap<String, Integer>(counter);
-            int start = i;
-            int j = i;
-            while (number.size() > 0 && j + length <= n && number.containsKey(s.substring(j, j + length))) {
-                c++;
-                String word = s.substring(j, j + length);
-                number.put(word, number.get(word) - 1);
-                if (number.get(word) == 0)
-                    number.remove(word);
-                j += length;
+            int size = number.size();
+            int i = k;
+            int start = k;
+            while (i < n) {
+                String word = s.substring(i, Math.min(n, i + wordLength));
+                if (number.containsKey(word)) {
+                    int value = number.get(word);
+                    if (value > 1) {
+                        number.put(word, value - 1);
+                    }
+                    else if (value == 1) {
+                        number.put(word, value - 1);
+                        size--;
+                        if (size == 0) {
+                            result.add(start);
+                            number.put(s.substring(start, start + wordLength), 1);
+                            start += wordLength;
+                            size = 1;
+                        }
+                    }
+                    else {
+                        int j = start;
+                        while (!word.equals(s.substring(j, j + wordLength))) {
+                            String startWord = s.substring(j, j + wordLength);
+                            value = number.get(startWord);
+                            if (value == 0) {
+                                size++;
+                            }
+                            number.put(startWord, value + 1);
+                            j += wordLength;
+                        }
+                        start = j + wordLength;
+                    }
+                    i += wordLength;
+                }
+                else {
+                    i += wordLength;
+                    if (n - i < totalLength) {
+                        break;
+                    }
+                    else {
+                        start = i;
+                        number = new HashMap<String, Integer>(counter);
+                        size = number.size();
+                    }
+                }
             }
-            if (number.isEmpty()) {
-                result.add(start);
-            }
-            
         }
-        System.out.println("number of loops: " + c);
+//        System.out.println("number of loops: " + c);
         return result;
     }
 }
-
