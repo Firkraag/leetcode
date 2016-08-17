cdbc9113a5ee23ab4bd21eff6a55ccd5a5c92f26
Firkraag <cntqrxj@gmail.com>
2016-08-17 15:59:15 +0800

    [335. Self Crossing][Accepted]committed by LeetCode Extension

--- 335-Self-Crossing/solution.py
+++ 335-Self-Crossing/solution.py
@@ -1,12 +1,33 @@
-class Solution(object):
-    def isSelfCrossing(self, x):
-        """
-        :type x: List[int]
-        :rtype: bool
-        """
-        b = c = d = e = 0
-        for a in x:
-            if d >= b > 0 and (a >= c or a >= c-e >= 0 and f >= d-b):
-                return True
-            b, c, d, e, f = a, b, c, d, e
-        return False
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& x) {
+        for(int i=3, l=x.size(); i<l; i++) {
+            // Case 1: current line crosses the line 3 steps ahead of it
+            if(x[i]>=x[i-2] && x[i-1]<=x[i-3]) return true;  
+            // Case 2: current line crosses the line 4 steps ahead of it
+            else if(i>=4 && x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2]) return true; 
+            // Case 3: current line crosses the line 6 steps ahead of it
+            else if(i>=5 && x[i-2]>=x[i-4] && x[i]+x[i-4]>=x[i-2] && x[i-1]<=x[i-3] && x[i-1]+x[i-5]>=x[i-3]) return true;  
+        }
+        return false;
+    }
+
+/*               i-2
+    case 1 : i-1┌─┐
+                └─┼─>i
+                 i-3
+                 
+                    i-2
+    case 2 : i-1 ┌────┐
+                 └─══>┘i-3
+                 i  i-4      (i overlapped i-4)
+
+    case 3 :    i-4
+               ┌──┐ 
+               │i<┼─┐
+            i-3│ i-5│i-1
+               └────┘
+                i-2
+
+*/
+};
