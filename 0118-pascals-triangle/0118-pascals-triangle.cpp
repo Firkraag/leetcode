defe0b7751775049a129f9311309e96f722b28c6
Firkraag <cntqrxj@gmail.com>
2023-10-25 00:50:59 +0800

    Time: 0 ms (100%), Space: 7.2 MB (13.29%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> values = {{}, {1},{1,1},{1,2,1},{1,3,3,1},{1,4,6,4,1}};
+        for (int i = values.size(); i <= numRows; i++) {
+            vector<int> row;
+            row.reserve(i);
+            auto &prevRow = values[i - 1];
+            row.push_back(1);
+            for (int j = 0; j < i - 2; j++) {
+                row.push_back(prevRow[j] + prevRow[j + 1]);
+            }
+            row.push_back(1);
+            values.push_back(row);
+        }
+        return {values.begin() + 1, values.begin() + numRows + 1};
+    }
+};
