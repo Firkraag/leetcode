f35b84e9a47d70de0a52998127a20ff1c8077955
Firkraag <cntqrxj@gmail.com>
2023-10-25 00:56:18 +0800

    Time: 3 ms (41.63%), Space: 7.3 MB (7.88%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> values = {{}, {1},{1,1},{1,2,1},{1,3,3,1},{1,4,6,4,1}};
-        for (int i = values.size(); i <= numRows; i++) {
+        vector<vector<int>> values = {{1},{1,1},{1,2,1},{1,3,3,1},{1,4,6,4,1}};
+        for (int i = values.size(); i < numRows; i++) {
             vector<int> row;
-            row.reserve(i);
+            row.reserve(i + 1);
             auto &prevRow = values[i - 1];
             row.push_back(1);
-            for (int j = 0; j < i - 2; j++) {
+            for (int j = 0; j < i - 1; j++) {
                 row.push_back(prevRow[j] + prevRow[j + 1]);
             }
             row.push_back(1);
             values.push_back(row);
         }
-        return {values.begin() + 1, values.begin() + numRows + 1};
+        return {values.begin(), values.begin() + numRows};
     }
 };
