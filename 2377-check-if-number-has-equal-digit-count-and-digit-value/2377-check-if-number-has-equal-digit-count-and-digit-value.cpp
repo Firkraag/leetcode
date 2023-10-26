90a62f0cf1bc595bcce68d39d17eb80622f4f395
Firkraag <cntqrxj@gmail.com>
2023-10-27 01:23:32 +0800

    Time: 0 ms (100%), Space: 6.4 MB (53.13%) - LeetHub

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool digitCount(string num) {
         int n = num.size();
-        int count[n];
-        for (int i = 0; i < n; i++) {
-            count[i] = 0;
-        }
+        int count[10] = {0};
         for (const char c: num) {
             if (c - '0' < n) {
                 count[c - '0']++;
