4407e304c188825d244fe22b1177176a71bca57f
Firkraag <cntqrxj@gmail.com>
2023-10-26 13:00:24 +0800

    Time: 0 ms (100%), Space: 8.9 MB (65.15%) - LeetHub

--- 1287-distance-between-bus-stops/1287-distance-between-bus-stops.cpp
+++ 1287-distance-between-bus-stops/1287-distance-between-bus-stops.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-  int distanceBetweenBusStops(vector<int> &distance, int start,
-                              int destination) {
-    int n = distance.size();
-    int sum1 = 0;
-    int sum2 = 0;
-    for (int i = start; i != destination; i = (i + 1) % n) {
-      sum1 += distance[i];
+  int distanceBetweenBusStops(vector<int> &distance, int start, int destination) {
+    if (start > destination) swap(start, destination);
+
+    int sum1 = 0; // Clockwise distance
+    int totalDistance = 0;
+
+    for (int i = 0; i < distance.size(); ++i) {
+      if (i >= start && i < destination) {
+        sum1 += distance[i];
+      }
+      totalDistance += distance[i];
     }
-    for (int i = start; i != destination;) {
-		i = ((i - 1) % n + n) % n;
-      sum2 += distance[i];
-    }
+
+    int sum2 = totalDistance - sum1; // Counter-clockwise distance
+
     return min(sum1, sum2);
   }
 };
