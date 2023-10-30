573c2571dd124fde9ed09cb249bb79eb2256a34a
Firkraag <cntqrxj@gmail.com>
2023-10-30 13:14:51 +0800

    Time: 0 ms (100%), Space: 6.6 MB (21.94%) - LeetHub

--- 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -1,71 +1,25 @@
+#include <string>
+#include <algorithm>
+
 class Solution {
 public:
-    string modifyString(string s) {
-       string ans;
-       int n = s.size();
-       if (n == 1) {
-           char c = s[0];
-           if (c == '?') {
-               ans.push_back('a');
-           }
-           else {
-               ans.push_back(c);
-           }
-           return ans;
-       }
-       for (int i = 0; i < n; i++) {
-           char c = s[i];
-           if (c != '?') {
-               ans.push_back(c);
-           }
-           else if (i == 0){
-               char next = s[i + 1];
-               if (next == '?') {
-                   ans.push_back('a');
-               }
-               else if (next == 'z') {
-                   ans.push_back('a');
-               }
-               else {
-                   ans.push_back(next + 1);
-               }
-           }
-           else if (i == n - 1) {
-               char prev = ans[i - 1];
-               if (prev == 'z') {
-                   ans.push_back('a');
-               }
-               else {
-                   ans.push_back(prev + 1);
-               }
-           }
-           else {
-               char prev = ans[i - 1];
-               char next = s[i + 1];
-               if (next == '?') {
-                   if (prev == 'z') {
-                      ans.push_back('a'); 
-                   }
-                   else {
-                       ans.push_back(prev + 1);
-                   }
-                   continue;
-               }
-               char minimum = min(prev, next);
-               char maximum = max(prev, next);
-               if (minimum + 1 >= maximum) {
-                   if (minimum == 'a') {
-                       ans.push_back('c');
-                   }
-                   else {
-                       ans.push_back('a');
-                   }
-               }
-               else {
-                   ans.push_back(minimum + 1);
-               }
-           }
-       }
-       return ans;
+    std::string modifyString(std::string s) {
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '?') {
+                char prev = (i == 0) ? '?' : s[i - 1];
+                char next = (i == n - 1) ? '?' : s[i + 1];
+
+                char replacement = 'a';
+                while (replacement == prev || replacement == next) {
+                    replacement++;
+                }
+
+                s[i] = replacement;
+            }
+        }
+
+        return s;
     }
 };
