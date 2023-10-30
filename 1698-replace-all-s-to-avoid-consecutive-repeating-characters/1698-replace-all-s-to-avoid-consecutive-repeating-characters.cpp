b9e216840d740008b4bd129fda4ee4700d26dffc
Firkraag <cntqrxj@gmail.com>
2023-10-30 13:19:41 +0800

    Time: 4 ms (29.5%), Space: 6.4 MB (42.45%) - LeetHub

--- 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/1698-replace-all-s-to-avoid-consecutive-repeating-characters.cpp
@@ -8,8 +8,8 @@ public:
 
         for (int i = 0; i < n; i++) {
             if (s[i] == '?') {
-                char prev = (i == 0) ? '?' : s[i - 1];
-                char next = (i == n - 1) ? '?' : s[i + 1];
+                char prev = (i == 0) ? 'z' : s[i - 1];
+                char next = (i == n - 1) ? 'z' : s[i + 1];
 
                 char replacement = 'a';
                 while (replacement == prev || replacement == next) {
