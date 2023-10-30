3c4f05ade2e4c16f77b4c54e7c223d1a8eb3149b
Firkraag <cntqrxj@gmail.com>
2023-10-31 00:29:26 +0800

    Time: 91 ms (66.55%), Space: 53.7 MB (68.25%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.cpp
@@ -4,8 +4,8 @@ public:
         sort(piles.begin(), piles.end(), greater<int>());
         int n = piles.size();
         int sum = 0;
-        for (int i = 0; i < n / 3; i++) {
-            sum += piles[2 * i + 1];
+        for (int i = 1; i < 2 * n / 3; i+=2) {
+            sum += piles[i];
         }
         return sum;
     }
