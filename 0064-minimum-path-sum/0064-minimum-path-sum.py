8c1dc936d188a9e9240882417086ba928d788fbb
Firkraag <cntqrxj@gmail.com>
2025-01-18 00:31:44 +0800

    Time: 27 ms (27.26%), Space: 20.4 MB (24.52%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        self.m = len(grid)
-        self.n = len(grid[0])
-        memorize = [[-1] * self.n for _ in range(self.m)]
+        m = len(grid)
+        n = len(grid[0])
+        memorize = [[-1] * n for _ in range(m)]
+        memorize[m - 1][n - 1] = grid[m - 1][n - 1]
+        for col in range(n - 2, -1, -1):
+            memorize[m - 1][col] = grid[m - 1][col] + memorize[m - 1][col + 1]
+        for row in range(m - 2, -1, -1):
+            memorize[row][n - 1] = grid[row][n - 1] + memorize[row + 1][n - 1]
+
         return self.minPathSumAux(grid, 0, 0, memorize)
     
     def minPathSumAux(self, grid, row, col, memorize):
         if memorize[row][col] != -1:
             return memorize[row][col]
-        if row >= self.m - 1 and col >= self.n - 1:
-            return grid[row][col]
-        elif row < self.m - 1 and col < self.n - 1:
-            memorize[row][col] = min(self.minPathSumAux(grid, row + 1, col, memorize), self.minPathSumAux(grid, row, col + 1, memorize)) + grid[row][col]
-        elif row < self.m - 1 and col == self.n - 1:
-            memorize[row][col] = self.minPathSumAux(grid, row + 1, col, memorize) + grid[row][col]
-        elif row == self.m - 1 and col < self.n - 1:
-            memorize[row][col] = self.minPathSumAux(grid, row, col + 1, memorize) + grid[row][col]
+        memorize[row][col] = min(self.minPathSumAux(grid, row + 1, col, memorize), self.minPathSumAux(grid, row, col + 1, memorize)) + grid[row][col]
         return memorize[row][col]
