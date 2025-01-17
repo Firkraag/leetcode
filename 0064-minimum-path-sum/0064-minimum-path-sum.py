b69457feee964a2ccf38bddf3bc63a24fc8349e6
Firkraag <cntqrxj@gmail.com>
2025-01-18 00:12:30 +0800

    Time: 50 ms (13.37%), Space: 20.5 MB (24.52%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        memorize = [[float("inf")] * n for _ in range(m)]
+        return self.minPathSumAux(grid, 0, 0, memorize)
+    
+    def minPathSumAux(self, grid, row, col, memorize):
+        m = len(grid)
+        n = len(grid[0])
+        if memorize[row][col] != float("Inf"):
+            return memorize[row][col]
+        if row >= m - 1 and col >= n - 1:
+            return grid[row][col]
+        elif row < m - 1 and col < n - 1:
+            memorize[row][col] = min(self.minPathSumAux(grid, row + 1, col, memorize), self.minPathSumAux(grid, row, col + 1, memorize)) + grid[row][col]
+        elif row < m - 1 and col == n - 1:
+            memorize[row][col] = self.minPathSumAux(grid, row + 1, col, memorize) + grid[row][col]
+        elif row == m - 1 and col < n - 1:
+            memorize[row][col] = self.minPathSumAux(grid, row, col + 1, memorize) + grid[row][col]
+        return memorize[row][col]
